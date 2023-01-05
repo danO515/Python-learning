e3a164c915a3f830ae6b456471e4e8454bc77775
Your Name <you@example.com>
2023-01-05 19:29:20 +0900

    second commit

--- 과학뉴스.py
+++ 과학뉴스.py
@@ -1,21 +1,63 @@
-
 import requests
+import datetime as dt
+x = dt.datetime.now()
 
 headers = \
 {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
 
-url = 'https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=731'
-site = requests.get(url, headers=headers)
-source_data = site.text
+for j in (0, 1, 2, 3, 4):
+    for i in (1, 2, 3):
+        if x.day - j < 1:
+            
+            if x.month - 1 < 1:
+                y = x.year - 1
+                m = 12
+            else:
+                y = x.year
+                m = x.month - 1
+            
+            if x.month == 2 | x.month == 4 | x.month == 6 | x.month == 9 | x.month == 11:
+                d = 30 + x.day - j
+            else:
+                d = 31 + x.day - j 
+        
+        else:
+            d = x.day - j
+            m = x.month
+            y = x.year
 
-count = source_data.count('width="106" height="72" alt="')
 
-for i in range(count):
-    pos1= source_data.find(' width="106" height="72" alt="') + len(' width="106" height="72" alt="')
-    source_data = source_data[pos1:]
+        if d < 10:
+            day = '{0}{1}'.format(0, d)
+        else:
+            day = x.day
+        
+        if m < 10:
+            month = '{0}{1}'.format(0, m)
+        else:
+            month = x.month
 
-    pos2 = source_data.find('onError')
-    extract_data = source_data[:pos2-2]
+        date = '{0}{1}{2}'.format(y, month, day)
 
-    source_data = source_data[pos2+1:]
-    print(i+1, extract_data)
+
+        url = 'https://news.naver.com/main/list.naver?mode=LS2D&sid2=731&sid1=105&mid=shm&date={0}&page={1}'.format(date, i)
+        site = requests.get(url, headers=headers)
+        source_data = site.text
+
+        count = source_data.count('width="106" height="72" alt=')
+
+        print()
+        print()
+        print("날짜 : {0}  페이지 : {1}\n".format(date, i))
+
+        for k in range(count):
+            pos1= source_data.find('width="106" height="72" alt=') + len('width="106" height="72" alt=')
+            source_data = source_data[pos1+1:]
+
+            pos2 = source_data.find('onError')
+            extract_data = source_data[:pos2-2]
+
+            source_data = source_data[pos2+1:]
+            print(k+1, extract_data)
+
+        
